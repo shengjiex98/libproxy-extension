fbf4de49afe2321a9eb2db335690711e23b32141
Shengjie Xu <shengjiex98@gmail.com>
2022-12-09 14:59:52 -0500

    New version without useless popup page

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.action.onClicked.addListener(tab => {
+    let url = tab.url
+    // Do nothing if already using UNC library proxy
+    if (url?.match('libproxy.lib.unc.edu'))
+        return
+    let newurl = 'https://libproxy.lib.unc.edu/login?url=' + url
+    chrome.tabs.update(tab.id || 0, {url: newurl});
+})
--- scripts/popup.ts
+++
@@ -1,9 +0,0 @@
-console.log("hi")
-
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    let url = tabs[0].url
-    // Do nothing if already using UNC library proxy
-    if (url?.match('libproxy.lib.unc.edu'))
-        return
-    chrome.tabs.update(tabs[0].id || 0, {url: 'https://libproxy.lib.unc.edu/login?url=' + url});
-})
